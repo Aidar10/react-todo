0aa45ceadcb7e9f0f83094f8bd1e5d1e7612853b
aidar_dev <studdar@yandex.ru>
2017-09-20 18:06:28 +0300

    Component restructure

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
 function Button(props) {
-    return(
-        <button className={props.className}>
+    return (
+        <button className={props.className} onClick={props.onClick}>
             <i className="material-icons">{props.icon}</i>
         </button>
     );
 }
 
-Button.propTypes={
+Button.propTypes = {
     className: React.PropTypes.string,
-    icon: React.PropTypes.string
+    icon: React.PropTypes.string,
+    onClick: React.PropTypes.func
 };
 
 export default Button;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import Stats from './stats';
 
 function Header(props) {
     return (
         <header>
+            <Stats todos={props.todos}/>
             <h1>{props.title}</h1>
         </header>
     );
 }
 
 Header.propTypes = {
-    title: React.PropTypes.string.isRequired
+    title: React.PropTypes.string.isRequired,
+    todos: React.PropTypes.arrayOf(React.PropTypes.shape({
+        id: React.PropTypes.number.isRequired,
+        title: React.PropTypes.string.isRequired,
+        completed: React.PropTypes.bool.isRequired
+    })).isRequired
+
 };
 
 export default Header;
